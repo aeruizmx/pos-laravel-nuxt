d9b2892be431a7087d89a818d340fae0fba4e213
aruiz <aruiz@grupooctano.com.mx>
2023-01-05 14:08:12 -0600

    Clase 23. Integrando Plantilla

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -12,7 +12,20 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/assets/css/nucleo-icons.css' },
+      { rel: 'stylesheet', href: '/assets/css/nucleo-svg.css' },
+      { rel: 'stylesheet', href: '/assets/css/soft-ui-dashboard.min.css?v=1.0.9' },
+    ],
+    script: [
+      { src:"../assets/js/core/popper.min.js" },
+      { src:"../assets/js/core/bootstrap.min.js" },
+      { src:"../assets/js/plugins/perfect-scrollbar.min.js" },
+      { src:"../assets/js/plugins/smooth-scrollbar.min.js" },
+
+      { src:"../assets/js/plugins/dragula/dragula.min.js" },
+      { src:"../assets/js/plugins/jkanban/jkanban.js" },
+      { src:"../assets/js/plugins/chartjs.min.js" },
     ]
   },
 
