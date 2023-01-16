fd3190d906f0d7f3822be83e48deb41720876b7b
aruiz <aruiz@grupooctano.com.mx>
2023-01-16 09:38:37 -0600

    Clase 24. Integrando Plantilla y Optimizando HTML

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -15,14 +15,15 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: '/assets/css/nucleo-icons.css' },
       { rel: 'stylesheet', href: '/assets/css/nucleo-svg.css' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' },
       { rel: 'stylesheet', href: '/assets/css/soft-ui-dashboard.min.css?v=1.0.9' },
     ],
     script: [
+      { src:"https://kit.fontawesome.com/42d5adcbca.js" },
       { src:"../assets/js/core/popper.min.js" },
       { src:"../assets/js/core/bootstrap.min.js" },
       { src:"../assets/js/plugins/perfect-scrollbar.min.js" },
       { src:"../assets/js/plugins/smooth-scrollbar.min.js" },
-
       { src:"../assets/js/plugins/dragula/dragula.min.js" },
       { src:"../assets/js/plugins/jkanban/jkanban.js" },
       { src:"../assets/js/plugins/chartjs.min.js" },
